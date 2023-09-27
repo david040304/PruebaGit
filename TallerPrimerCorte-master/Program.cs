8bb5c2732a893d4474a54f17382208b936420c8d
JOSE DAVID MUÑOZ <jdmunozfernandez@unicesar.edu.co>
2023-09-26 19:12:05 -0500

    Actualizacion

---
+++ TallerPrimerCorte-master/Program.cs
@@ -0,0 +1,22 @@
+﻿using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidad;
+
+namespace Presentacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta Juan= new Cuenta(1,2333,"123456");
+
+
+            
+            
+        }
+    }
+}
